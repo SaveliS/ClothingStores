4d9dc804e38d65eba72596de920086e7b95f8af6
Saveli <tuponogov.90@mail.ru>
2023-08-20 20:34:30 +0300

    Настроен фильтр на принятие css, js. Интерфейс для ввода почты.

--- src/main/java/com/massage/massageK/config/SecurityConfig.java
+++ src/main/java/com/massage/massageK/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         return http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/").permitAll())
+                        .requestMatchers("/","/css/**","/js/**").permitAll())
                 .build();
     }
 }
